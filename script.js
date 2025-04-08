79b55269b4c1cc19f6c3c9fdd0424dbf0df78020
Narsa011 <swroopsingh6376@gmail.com>
2025-04-07 21:43:56 -0700

    hi

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMode() {
+  document.body.classList.toggle("dark");
+}
+
+function toggleMenu() {
+  const navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("show");
+}
